df52155e14a1c8b0925882b0793a7e226dea4f73
Adam Zieliński <adam@adamziel.com>
2026-04-29 00:53:52 +0200

    Relax Darwin ZFS mutex teardown shim

--- forkpress/zfs-engine/forkpress_zfs.c
+++ forkpress/zfs-engine/forkpress_zfs.c
@@ -47,13 +47,11 @@
 #undef pthread_mutex_destroy
 #endif
 
-static int forkpress_zfs_finalizing = 0;
-
 int
 forkpress_zfs_pthread_mutex_destroy(pthread_mutex_t *mutex)
 {
     int err = pthread_mutex_destroy(mutex);
-    if (forkpress_zfs_finalizing && (err == EINVAL || err == EBUSY))
+    if (err == EINVAL || err == EBUSY)
         return (0);
     return (err);
 }
@@ -156,13 +154,7 @@ forkpress_zfs_init(void)
 int
 forkpress_zfs_fini(void)
 {
-#if defined(__APPLE__)
-    forkpress_zfs_finalizing = 1;
-#endif
     kernel_fini();
-#if defined(__APPLE__)
-    forkpress_zfs_finalizing = 0;
-#endif
     return (0);
 }
 
